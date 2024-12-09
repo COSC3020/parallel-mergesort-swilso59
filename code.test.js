8ff5c1ff8b66a56fe98ec45d0a6fa2d950ced0c3
Sean W. <104464288+swilso59@users.noreply.github.com>
2024-12-08 17:11:23 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const jsc = require('jsverify');
+
+eval(fs.readFileSync(' ./code.js') + '');
+
+const testSort = jsc.forall("array nat", async function (arr) {
+    const a1 = JSON.parse(JSON.stringify(arr));
+    const a2 = JSON.parse(JSON.stringify(arr));
+    const sortedParallel = await parallelMergesort(a1); 
+    const sortedNative = a2.sort((a, b) => a - b); 
+    return JSON.stringify(sortedParallel) === JSON.stringify(sortedNative);
+});
+
+jsc.assert(testSort);
+
 
